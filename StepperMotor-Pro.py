069b15d4899b6d4a522656ee2936738002e7d7a2
swayamtayaliabits <25f3001345@ds.study.iitm.ac.in>
2026-02-16 16:38:46 +0530

    Update StepperMotor-Pro.py

--- StepperMotor-Pro.py
+++ StepperMotor-Pro.py
@@ -1,1 +1,24 @@
+from machine import Pin
+import time
+IN1 = Pin(5,Pin.OUT)
+IN2 = Pin(14,Pin.OUT)
+IN3 = Pin(18,Pin.OUT)
+IN4 = Pin(19,Pin.OUT)
+List1 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+List1rev = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+while True:
+    for i in range(500):
+        for j in List1:
+            IN1.value(i[0])
+            IN1.value(i[1])
+            IN1.value(i[2])
+            IN1.value(i[3])
+            time.sleep_ms(5)
 
+    for i in range(500):
+        for k in List1rev:
+            IN1.value(i[0])
+            IN1.value(i[1])
+            IN1.value(i[2])
+            IN1.value(i[3])
+            time.sleep_ms(5)
