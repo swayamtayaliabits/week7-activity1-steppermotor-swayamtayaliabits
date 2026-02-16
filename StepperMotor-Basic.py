523497154560da56a68b7477d40e2989e77b8d8d
swayamtayaliabits <25f3001345@ds.study.iitm.ac.in>
2026-02-16 14:05:09 +0530

    Update StepperMotor-Basic.py

--- StepperMotor-Basic.py
+++ StepperMotor-Basic.py
@@ -1,1 +1,30 @@
-#Write your code here to run the stepper motor without using any loop
+from machine import Pin
+import time
+IN1 = Pin(5,Pin.OUT)
+IN2 = Pin(14,Pin.OUT)
+IN3 = Pin(18,Pin.OUT)
+IN4 = Pin(19,Pin.OUT)
+while True:
+    IN1.value(1)
+    IN2.value(0)
+    IN3.value(0)
+    IN4.value(0)
+    time.sleep_ms(5)
+    IN1.value(0)
+    IN2.value(1)
+    IN3.value(0)
+    IN4.value(0)
+    time.sleep_ms(5)
+    IN1.value(0)
+    IN2.value(0)
+    IN3.value(1)
+    IN4.value(0)
+    time.sleep_ms(5)
+    IN1.value(0)
+    IN2.value(0)
+    IN3.value(0)
+    IN4.value(1)
+    time.sleep_ms(5)
+
+
+
